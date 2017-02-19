e7f4793673e482161d9bf5e7898961852116ef03
Alexander <lss1ash@rambler.ru>
2017-02-20 08:09:12 +1100

    Анимация добавлена

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@
 var feedback_button = document.querySelector(".feedback");
 var modal = document.querySelector(".modal");
 var modal_close = document.querySelector(".modal_close");
+var modal_form = document.querySelector(".modal form");
 
 feedback_button.addEventListener("click", function(event){
   event.preventDefault();
   modal.classList.toggle("modal_hidden"); // Отобразим окошко
+  modal_form.classList.toggle("modal_animated"); // Анимируем окошко
 });
 
 modal_close.addEventListener("click", function(event){
   event.preventDefault();
   modal.classList.toggle("modal_hidden"); // Спрячем окошко
+  modal_form.classList.toggle("modal_animated"); // Уберём анмацию окошка
 });
